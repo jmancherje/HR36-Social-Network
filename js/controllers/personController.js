b79cf5939c687d7fd44ca223d31cf22a9f7bf531
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 15:58:28 -0800

    dynamic routing to different user pages now fully functional. Basic MVP complete

--- js/controllers/personController.js
+++ js/controllers/personController.js
@@ -1,5 +1,11 @@
 angular.module('app.person', [])
 
-.controller('PersonController', function ($scope) {
+.controller('PersonController', function ($scope, $http, $routeParams) {
   $scope.person = 'Someone\'s name';
+  $http.get('../js/students.json').success(function (data) {
+    $scope.allStudents = data;
+    $scope.studentId = $routeParams.itemId;
+    $scope.student = data[$scope.studentId];
+  });
+
 });
