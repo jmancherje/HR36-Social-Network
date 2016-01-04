2bb72c7520d377e70f4a6c7df612d289d84bd679
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 11:58:51 -0800

    add more placeholder people

--- js/controllers/listController.js
+++ js/controllers/listController.js
@@ -11,6 +11,21 @@ angular.module('app.list', [])
       "name":"Jane Someone",
       "location":"New York, New York",
       "info":"Hates to do stuff"
-    }
+    },
+    {
+      "name":"Mutton Padre",
+      "location":"London, England",
+      "info":"Dresses up dogs like horses"
+    },
+    {
+      "name":"Jaramiah Wilson",
+      "location":"Aukland, New Zealand",
+      "info":"A wizard on the weekends"
+    },
+    {
+      "name":"Sir Francis Columbo",
+      "location":"Madrid, Spain",
+      "info":"Likes to explore on boats"
+    },
   ]
 });
