e41af99db1464f078a0e1deec36f6cb765442a3e
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 11:50:21 -0800

    separate controller to new file without breaking functionality

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-angular.module('app', [])
+angular.module('app', ['app.list']);
 
 // // add controller reference
-.controller('list-controller', function ($scope) {
-  $scope.people = [
-    {
-      "name":"John Doe",
-      "location":"San Francisco, California",
-      "info":"Likes to do stuff"
-    },
-    {
-      "name":"Jane Someone",
-      "location":"New York, New York",
-      "info":"Hates to do stuff"
-    }
-  ]
-});
+// .controller('list-controller', function ($scope) {
+//   $scope.people = [
+//     {
+//       "name":"John Doe",
+//       "location":"San Francisco, California",
+//       "info":"Likes to do stuff"
+//     },
+//     {
+//       "name":"Jane Someone",
+//       "location":"New York, New York",
+//       "info":"Hates to do stuff"
+//     }
+//   ]
+// });
 // // add route provider
