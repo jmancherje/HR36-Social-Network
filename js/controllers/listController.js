c2d855ea75998fcc4f4671ad0fc70e5080ba26c6
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 15:12:04 -0800

    remove dummy data from list controller

--- js/controllers/listController.js
+++ js/controllers/listController.js
@@ -1,34 +1,6 @@
 angular.module('app.list', ['ngAnimate'])
 
 .controller('ListController', function ($scope) {
-  $scope.people = [
-    {
-      "name":"John Doe",
-      "location":"San Francisco, California",
-      "info":"Likes to do stuff"
-    },
-    {
-      "name":"Jane Someone",
-      "location":"New York, New York",
-      "info":"Hates to do stuff"
-    },
-    {
-      "name":"Mutton Padre",
-      "location":"London, England",
-      "info":"Dresses up dogs like horses"
-    },
-    {
-      "name":"Jaramiah Wilson",
-      "location":"Aukland, New Zealand",
-      "info":"A wizard on the weekends"
-    },
-    {
-      "name":"Sir Francis Columbo",
-      "location":"Madrid, Spain",
-      "info":"Likes sail boats"
-    },
-  ]
-
   $scope.students = [
     {
       "name":"AJ Salaudeen",
