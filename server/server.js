998f1b6fc75312aa03122a741f159b9de04bbbd0
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 17:21:40 -0800

    update node modules and node dependencies in package.json

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/../public'));
 
