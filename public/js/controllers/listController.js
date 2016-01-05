0b187c1b34bc97f39c9a7e3a60c1ab4686a29a17
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-04 20:33:20 -0800

    add login functionality in listController and list html

--- public/js/controllers/listController.js
+++ public/js/controllers/listController.js
@@ -1,8 +1,37 @@
 angular.module('app.list', ['ngAnimate'])
 
+// .factory('listService', function ($http) {
+//   var login = function (username) {
+//     $http.post('/users', username)
+//         .then(function (res) {
+//           sessionStorage.setItem('key', 'value');
+//         }, function () {
+//           console.log('error in posting to users');
+//         })
+//   }
+// })
+
 .controller('ListController', function ($scope, $http) {
   $http.get('js/students.json').success(function (data) {
     $scope.students = data;
   });
+
+  $scope.user = sessionStorage.getItem('username');
+
+  $scope.login = function () {
+    $http.post('/users', $scope.username)
+    .then(function (res) {
+      sessionStorage.setItem('username', $scope.username);
+      $scope.user = sessionStorage.getItem('username');
+      console.log('username stored in session ');
+    }, function () {
+      console.log('error in posting to users');
+    })
+  };
+
+  $scope.logout = function () {
+    sessionStorage.removeItem('username');
+    $scope.user = null;
+  };
 
 });
